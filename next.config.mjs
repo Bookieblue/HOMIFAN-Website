6ef9004a89c8f3cc93a64f17818e5c3eff0bed38
Bookieblue <oluwabukola.fasan@gmail.com>
2024-05-26 02:43:53 +0100

    dashboard

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:transactions*',
+            destination: 'https://v2.base-borderless.com/api/:transactions*',
+          },
+        ]
+      },
+};
 
 export default nextConfig;
