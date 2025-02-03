d82571505b39b8c8f20a6b8db87b7ae47ecfdd10
Masud Ndatsu <masudndatsu@gmail.com>
2025-02-03 18:11:28 +0100

    fix: handled cors error

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://v2.base-borderless.com/api/:path*',
-  //     },
-  //   ];
-  // },
+    // async rewrites() {
+    //   return [
+    //     {
+    //       source: '/api/:path*',
+    //       destination: 'https://v2.base-borderless.com/api/:path*',
+    //     },
+    //   ];
+    // },
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PUT,DELETE,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type, Authorization",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
