531f7e4741d1f8f653a5cccb2e0f1d4ec231f417
Masud-Ndatsu <masudndatsu@gmail.com>
2025-02-25 11:13:36 +0100

    fix: fixed prayerRequests typo

--- app/api/donations/verify/[reference]/route.ts
+++ app/api/donations/verify/[reference]/route.ts
@@ -6,8 +6,60 @@ import {
 import { formatZodError } from "@/app/utils/formatter";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import axios, { AxiosResponse } from "axios";
 
 const referenceSchema = z.string();
+
+interface PaystackVerificationResponse {
+  status: boolean;
+  message: string;
+  data: {
+    id: number;
+    amount: number;
+    status: string;
+    reference: string;
+    customer: {
+      email: string;
+      name: string;
+    };
+    // Add other fields as needed
+  };
+}
+
+async function verifyPaystackPayment(
+  secretKey: string,
+  transactionReference: string
+): Promise<PaystackVerificationResponse | null> {
+  // Paystack API endpoint for transaction verification
+  const url = `https://api.paystack.co/transaction/verify/${transactionReference}`;
+
+  // Set up the headers with the secret key
+  const headers = {
+    Authorization: `Bearer ${secretKey}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    // Make the GET request to Paystack API
+    const response: AxiosResponse<PaystackVerificationResponse> =
+      await axios.get(url, { headers });
+
+    // Return the JSON response
+    return response.data;
+  } catch (error: any) {
+    // Handle any errors that occur during the request
+    if (axios.isAxiosError(error)) {
+      console.error("Axios error:", error.response?.data || error.message);
+    } else {
+      console.error("Unexpected error:", error);
+    }
+    return {
+      status: false,
+      message: "Failed to verify payment",
+      data: error.response?.data || error.message,
+    };
+  }
+}
 
 export const GET = async (
   _request: Request,
@@ -34,9 +86,22 @@ export const GET = async (
       return sendErrorResponse(NextResponse, "Oops...Donation not found!", 404);
     }
 
+    const response = await verifyPaystackPayment(
+      process.env.PAYSTACK_SECRET_KEY!,
+      reference
+    );
+
+    if (!response?.status) {
+      return sendErrorResponse(
+        NextResponse,
+        JSON.stringify(response?.data),
+        400
+      );
+    }
+
     const updatedRecord = await prisma.donation.update({
       where: { id: donation.id },
-      data: { paymentStatus: "completed" },
+      data: { paymentStatus: response.data.status },
     });
 
     return sendSuccessResponse(
@@ -46,7 +111,6 @@ export const GET = async (
     );
   } catch (error) {
     console.error("Error retrieving member:", error);
-
     throw error;
   }
 };
